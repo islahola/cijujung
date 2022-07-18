e923e043be8c1b6f0dd57e9cb32f8bfb25cbf89f
islahola <islah.khofifah@gmail.com>
2022-07-19 01:54:16 +0700

    heroku

--- resources/views/layouts/partials/contentprofildesa.blade.php
+++ resources/views/layouts/partials/contentprofildesa.blade.php
@@ -4,3 +4,19 @@
 @include('layouts.partials.keanggotaan')
 @include('layouts.partials.misi')
 
+<!-- ini nanti buat dimasing2 page -->
+<!-- 
+<img src="{{$profil->foto_desa}}" alt="">
+<p>{{$profil->nama_desa}}</p>
+<p>{{$profil->sejarah}}</p>
+<p>{{$profil->visi}}</p>
+<p>{{$profil->misi}}</p>
+
+@foreach($struktur_pemerintahan as $data)
+    <img src="{{asset('upload/' . $data->foto_pengurus)}}" alt="">
+    <p>{{$data->nama}}</p>
+    <p>{{$data->jabatan}}</p>
+    <p>{{$data->tempat_lahir}}, {{$data->tanggal_lahir}}</p>
+    <p>{{$data->pendidikan}}</p>
+    <p>{{$data->nip}}</p>
+@endforeach -->
--- resources/views/layouts/partials/sejarah.blade.php
+++ resources/views/layouts/partials/sejarah.blade.php
@@ -3,13 +3,13 @@
     <h3>Cerita di balik Desa Cijujung</h3>
   </div>
   <div class="d-flex  sejarah__wrap">
-    <div class="col-6 col-sm-12  col-md-12 col-lg-7  sejarah__teks">
+    <div class="  col-md-12 col-lg-7 col-sm-10   sejarah__teks">
       <p >Cijujung adalah sebuah desa di kecamatan Cibungbulang, Kabupaten Bogor, Jawa Barat, Indonesia. Masyarakat pedekik mayoritas beretnis Jawa. Dan urutan kedua terbanyak di tempati etnis Melayu. Namun begitu ada juga etnis atau suku lainnya yang tinggal di Desa ini walaupun tidak banyak. Jadi penduduk atau warga Pedekik pada dasarnya sudah heterogen. Untuk lebih jelasnya silahkan lihat tabel suku atau etnis berikut :
         <br><br>brHal ini mungkin disebabkan karena Pulau Bengkalis dahulu merupakan wilayah Kerajaan Melayu Siak. Disamping itu yang terlebih dahulu menempati Pulau Bengkalis umumnya dan sebagian wilayah desa Pedekik khususnya adalah suku Melayu. Namun demikian adat istiadat dan budaya Jawa tetap masih kelihatan dalam berbagai hal khususnya di wilayah Dusun II, III dan IV yang warganya mayoritas etnis Jawa.
         Setiap rumah yang mendapat giliran akan menyediakan makanan khas Lebaran yaitu ketupat disertai dengan berbagai macam lauk pauk dan berbagai jenis minuman. Karena banyaknya rumah yang dikunjungi, acara ini berlangsung minimal lima hari berturut-turut.
       </p>
     </div>
-    <div class=" col-sm-12  col-md-12 col-lg-4 sejarah__image  ">
+    <div class=" col-md-12 col-lg-4 col-sm-10  sejarah__image  ">
       <img src="https://bogor.apakabarnews.com/wp-content/uploads/2021/06/3F48EE89-A39C-4D4E-AAA8-579369128FB5-e1623951856608.jpeg" alt="sejarah cijujung kota cijujun">
     </div>
   </div>
