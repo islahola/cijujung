79f6bca72b69f542ef8ca88fa189a538b33a1e2b
islahola <islah.khofifah@gmail.com>
2022-08-08 22:44:02 +0700

    update berita

--- resources/views/layouts/partials/heroTentang.blade.php
+++ resources/views/layouts/partials/heroTentang.blade.php
@@ -1,5 +1,5 @@
 <div class="heroTentang mt-4">
   <div class="bannerTentang bg-light d-flex align-items-center ">
-    <h1 class="container text-center text-white">Memberikan pengalaman terbaik bagi seluruh Masyarakat dan Turis adalah komitmen Desa Cijujung</h1>
+    <h1 class="container text-center text-white">Memberikan Pengalaman Terbaik Bagi Seluruh Masyarakat dan Turis adalah Komitmen Desa Cijujung</h1>
   </div>
 </div>
--- resources/views/layouts/partials/sejarah.blade.php
+++ resources/views/layouts/partials/sejarah.blade.php
@@ -5,22 +5,22 @@
   <div class="d-flex  sejarah__wrap">
     <div class="  col-md-12 col-lg-7 col-sm-10   sejarah__teks">
       <p>
-      Desa Cijujung  merupakan salah salah satu Desa Diwilayah Kecamatan Cibungbulang Kabupaten Bogor dengan Luas Wilayah 304.218 Hektar yang terbagi dalam 4 (Empat) Dusun  10 ( Sepuluh) Rukun Warga  ( RW ) 25 ( Dua Puluh Lima ) Rukun Tetangga ( RT ) Batas wilayah Desa adalah  sebagai berikut
+      Desa Cijujung  merupakan salah salah satu Desa di wilayah Kecamatan Cibungbulang Kabupaten Bogor dengan luas wilayah 304.218 Hektar yang terbagi dalam 4 (Empat) Dusun  10 (Sepuluh) Rukun Warga  (RW) 25 (Dua Puluh Lima) Rukun Tetangga (RT) batas wilayah desa adalah  sebagai berikut
       </p>
       <ol>
-        <li>Sebelah Utara berbatasan dengan Desa 		: Karehkel</li>
-        <li>Sebelah Timur berbatasan dengan Desa 		: Ciaruteun ilir</li>
-        <li>Sebelah Selatan berbatasan dengan Desa 	: Cimanggu I,Cijujung,Galuga</li>
-        <li>Sebelah Barat berbatasan dengan Desa 		: Karehkel</li>
+        <li>Sebelah Utara berbatasan dengan Desa Karehkel</li>
+        <li>Sebelah Timur berbatasan dengan Desa Ciaruteun ilir</li>
+        <li>Sebelah Selatan berbatasan dengan Desa Cimanggu I, Cijujung, Galuga</li>
+        <li>Sebelah Barat berbatasan dengan Desa Karehkel</li>
       </ol>
       <p>
       Jarak Kantor Desa dengan Kantor Kecamatan Cibungbulang Kabuaten Bogor Provinsi Jawa Barat dan Ibu Kota Negara adalah sebagai berikut :
       </p>
       <ol>
-        <li> Kantor Kecamatan Cibungbulang 		:  4 	km</li>
-        <li>Kabupaten Bogor					:  49 	km</li>
-        <li>Provinsi Jawa Barat 				:  165 	km</li>
-        <li>Ibu Kota Negara					:  79,5 	km</li>
+        <li> Kantor Kecamatan Cibungbulang 	  4 	km</li>
+        <li>Kabupaten Bogor				  49 	km</li>
+        <li>Provinsi Jawa Barat 			  165 	km</li>
+        <li>Ibu Kota Negara				  79,5 	km</li>
       </ol>
     </div>
     <div class=" col-md-12 col-lg-4 col-sm-10  sejarah__image  ">
